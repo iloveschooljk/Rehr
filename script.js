f2b98506e42464c8985e04c36e70cc7e60c41c9f
iloveschooljk <103543624+iloveschooljk@users.noreply.github.com>
2025-04-03 17:20:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function showMessage() {
-    document.getElementById("message").textContent = "Hello, GitHub Pages!";
-}
+document.querySelector(".cloaker").addEventListener("click", function() {
+    alert("Cloaker feature coming soon!");
+});
